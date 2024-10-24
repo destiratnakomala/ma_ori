f992f1862d86593bedc568d3d6c04d7acd3062e0
Desti Ratna Komala <124844924+destiratnakomala@users.noreply.github.com>
2024-10-24 11:51:08 +0700

    Add files via upload

---
+++ ner copy 6.py
@@ -0,0 +1,304 @@
+from flask import Flask, request, render_template, redirect, send_from_directory
+import os
+import pandas as pd
+from PyPDF2 import PdfReader
+from pdfminer.high_level import extract_text
+from openai import OpenAI
+import json
+import datetime
+
+app = Flask(__name__)
+api_key = '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'
+UPLOAD_FOLDER = 'static'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+def count_pdfs(directory):
+    pdf_count = sum(1 for file in os.listdir(directory) if file.lower().endswith('.pdf'))
+    return pdf_count
+
+def count_pdfs_today(directory):
+    today = datetime.date.today()
+    pdf_count = sum(1 for file in os.listdir(directory) if file.lower().endswith('.pdf') and
+                    datetime.date.fromtimestamp(os.path.getmtime(os.path.join(directory, file))) == today)
+    return pdf_count
+
+def count_pdfs_last_three_months(directory):
+    today = datetime.date.today()
+    start_of_month = today.replace(day=1)
+    three_months_ago = start_of_month - datetime.timedelta(days=3 * 30)
+    pdf_count = sum(1 for file in os.listdir(directory) if file.lower().endswith('.pdf') and
+                    three_months_ago <= datetime.date.fromtimestamp(os.path.getctime(os.path.join(directory, file))) <= today)
+    return pdf_count
+
+def extract_text_from_pdf(pdf_path, start_page, end_page):
+    text = extract_text(pdf_path, page_numbers=range(start_page, end_page + 1))
+    return text
+
+template = """
+# Anda Adalah Seorang Hakim Agung Di Mahkamah Agung Di Indonesia. Berdasarkan Putusan Di Bawah Ini, Berikan Kesimpulannya:
+{}
+Variabel Yang Harus Ada Adalah Sebagai Berikut:
+
+## Kesimpulan
+- Kesimpulan: 'Nama Terdakwa, usia, dinyatakan bersalah melanggar Pasal Dakwaan berdasarkan putusan Lembaga Peradilan nomor Nomor Putusan Akhir yang terdaftar pada tanggal Tanggal Registrasi. Pengadilan memutuskan Hasil Putusan Akhir, dan menjatuhkan hukuman Hukuman.'
+
+## Informasi Perkara
+- Nomor Perkara: 
+- Nama Pengadilan Tinggi: 
+- Nama Pengadilan Negeri: 
+- Tingkat Proses: 
+
+## Terdakwa
+  - Terdakwa: 
+  - Jaksa Penuntut Umum: 
+  - Penasehat Hukum: 
+- Tanggal Sidang:
+- Tempat Sidang: 
+- Hakim yang Mengadili:
+  - Hakim Ketua: 
+  - Hakim Anggota: 
+  - Hakim Ad Hoc (jika ada): 
+- Pengajuan Kasasi Oleh: 
+- Alasan Pengajuan Kasasi: 
+
+## Ringkasan Perkara
+- Jenis Perkara:
+- Masalah Hukum Spesifik: 
+- Ringkasan Kasus: Gambaran singkat mengenai kasus, termasuk peristiwa utama yang menyebabkan persidangan.
+- Kronologi Kejadian: Rincian urutan kejadian terkait perkara, mulai dari insiden awal hingga sidang terakhir.
+
+## Putusan Pengadilan Negeri
+- Nomor Putusan: 
+- Nama Hakim: 
+- Hasil Putusan: 
+  - Jenis Hukuman: 
+  - Pasal-Pasal yang Dilanggar: 
+  - Penjelasan Singkat Putusan: 
+  - Tanggal Putusan: 
+  - Fakta yang Terbukti: 
+  - Analisis Hukum: 
+  - Faktor yang memberatkan: 
+  - Faktor yang meringankan: 
+
+
+## Putusan Pengadilan Tinggi
+- Nomor Putusan: 
+- Nama Hakim: 
+- Hasil Putusan: 
+  - Jenis Hukuman: 
+  - Pasal-Pasal yang Dilanggar: 
+  - Penjelasan Singkat Putusan: 
+  - Tanggal Putusan: 
+  - Fakta yang Terbukti: 
+  - Analisis Hukum: 
+  - Faktor Pemberat: 
+  - Faktor Peringan: 
+
+
+## Putusan Mahkamah Agung
+- Nomor Putusan: 
+- Nama Hakim: 
+- Hasil Putusan: 
+  - Jenis Hukuman: 
+  - Pasal-Pasal yang Dilanggar: 
+  - Penjelasan Singkat Putusan: 
+  - Tanggal Putusan: 
+  - Fakta yang Terbukti: 
+  - Analisis Hukum: 
+  - Hal yang memberatkan: 
+  - Hal yang meringankan: 
+
+
+
+## Analisis Hukum
+- Penerapan Hukum: Penjelasan tentang bagaimana hukum yang relevan diterapkan pada fakta-fakta yang terbukti dalam kasus ini.
+- Pasal-Pasal yang Dilanggar: Pasal-pasal hukum yang ditemukan telah dilanggar.
+- Interpretasi dan Penafsiran Hukum: Penjelasan mengenai interpretasi hukum oleh pengadilan dan alasan di balik penerapannya.
+- Pertimbangan dari Putusan Sebelumnya: Referensi terhadap preseden atau putusan sebelumnya yang dipertimbangkan dalam membuat keputusan.
+- Pertimbangan terhadap Argumentasi Kasasi: Pembahasan mengenai validitas argumen yang diajukan dalam kasasi dan bagaimana hal itu ditangani.
+
+## Faktor-Faktor yang Memberatkan dan Meringankan
+- Faktor-Faktor yang Memberatkan: Faktor-faktor yang menyebabkan hukuman menjadi lebih berat, seperti tingkat keparahan kejahatan atau catatan pelanggaran sebelumnya.
+- Faktor-Faktor yang Meringankan: Keadaan yang dapat menyebabkan hukuman menjadi lebih ringan, seperti penyesalan terdakwa atau kerjasama dengan pihak berwenang.
+
+## Amar Putusan
+- Jenis Hukuman yang Diminta: Rincian hukuman yang diminta oleh jaksa penuntut umum, termasuk jenis dan lamanya hukuman.
+- Permintaan Tambahan: Catatan mengenai hukuman tambahan atau tindakan lain yang diminta oleh jaksa.
+
+
+## Penutup
+- Tanggal Putusan: 
+- Komentar Terakhir Hakim: Pernyataan penutup atau komentar yang diberikan oleh hakim ketua.
+- Tanda Tangan Hakim:
+  - Hakim Ketua: 
+  - Hakim Anggota: 
+  - Panitera: 
+  - Hakim Ad Hoc (jika ada): 
+
+
+## Daftar Barang Bukti
+- Barang Bukti:Daftar terperinci dari semua barang yang diajukan sebagai bukti, beserta deskripsinya.
+  
+## Transkrip dan Catatan
+- Transkrip Pengadilan:Ringkasan transkrip pengadilan atau kutipan penting dari persidangan.
+  
+## Dokumentasi Tambahan
+- Dokumen Terkait:Dokumen tambahan atau referensi hukum lainnya yang menjadi bagian dari berkas perkara.
+
+
+Keterangan:
+- Urutan Tingkat Proses dari yang terendah sampai yang tertinggi Tingkat Pertama < Tingkat Banding < Tingkat Kasasi, Peninjauan Kembali (PK)
+- Tingkat Kasasi dan Peninjauan Kembali (PK) memiliki tingkatan yang sama namun keduanya berbeda baik dari prosedur maupun amar putusannya
+
+- Informasi 'Tingkat Proses' dan 'Lembaga Peradilan' selalu berkaitan dengan 'Nomor Putusan', dengan contoh sebagai berikut:
+    Jika contoh 'Nomor Putusan == '158/Pid.B/2023/PN Arm' terdapat informasi 'PN' menunjukan 'Lembaga Peradilan' == Pengadilan Negeri dan 'Tingkat Proses' == 'Tingkat Pertama'
+    Jika contoh 'Nomor Putusan == '31/PID.SUS/2024/PT DPS' terdapat informasi 'PT' menunjukan 'Lembaga Peradilan' == Pengadilan Tinggi dan 'Tingkat Proses' == 'Tingkat Banding'
+    Jika contoh 'Nomor Putusan == '1993 K/PID.SUS/2019' terdapat informasi 'K' menunjukan 'Tingkat Proses' == 'Tingkat Banding' dan 'Lembaga Peradilan' == Mahkamah Agung
+    Jika contoh 'Nomor Putusan == '156 PK/Pid.Sus/2019' terdapat informasi 'PK' menunjukan 'Tingkat Proses' == 'Peninjauan Kembali' dan 'Lembaga Peradilan' == Mahkamah Agung
+
+Jika 'Tingkat Proses' == Tingkat Pertama:
+    - 'Amar Putusan Pengadilan Negeri dicirikan dengan kalimat setelah kata "MENGADILI: ..." atau "M E N G A D I L I" tolong isikan dengan detail catatan Amar nya'
+
+Jika 'Tingkat Proses' == 'Tingkat Banding':
+    - 'Amar Putusan Pengadilan Tinggi dicirikan dengan kalimat setelah kata "MENGADILI: ..." atau "M E N G A D I L I" atau "MENGADILI SENDIRI" atau "MENGADILI KEMBALI" tolong isikan dengan detail Amar nya'
+    - 'Amar Putusan Pengadilan Negeri dicirikan dengan kalimat setelah kata "Membaca Putusan Pengadilan Negeri [Nama Kota] Nomor [Nomor Putusan ...]" tolong isikan dengan detail Amar nya'
+
+Jika 'Tingkat Proses == 'Tingkat Kasasi' dan 'Tingkat Proses == 'Peninjauan Kembali':
+    - 'Amar Putusan Pengadilan Mahkamah Agung dicirikan dengan kalimat setelah kata "MENGADILI: ..." atau "M E N G A D I L I" atau "MENGADILI SENDIRI" atau "MENGADILI KEMBALI" tolong isikan dengan detail catatan Amar nya'
+    - 'Amar Putusan Pengadilan Tinggi dicirikan dengan kalimat setelah kata "Membaca Putusan Pengadilan Tinggi [Nama Kota] Nomor [Nomor Putusan ...]" tolong isikan dengan detail catatan Amar nya'
+    - 'Amar Putusan Pengadilan Negeri dicirikan dengan kalimat setelah kata "Membaca Putusan Pengadilan Negeri [Nama Kota] Nomor [Nomor Putusan ...]" tolong isikan dengan detail catatan Amar nya'
+
+
+### Format "Kesimpulan":
+- Template Format:
+    ```
+    Terdakwa bernama 'Nama Terdakwa', usia 'Usia Terdakwa', dinyatakan bersalah melanggar 'Pasal Dakwaan', dituntut oleh 'lembaga putusan' total selama 'berapa tahun'  berdasarkan putusan 'Lembaga Peradilan' nomor 'Nomor Putusan Akhir' yang terdaftar pada tanggal 'Tanggal Registrasi'. Pengadilan memutuskan 'Hasil Putusan Akhir', dan menjatuhkan hukuman 'Hukuman'.'
+    ```
+- Example:
+    ```
+    'John Doe, usia 35, dinyatakan bersalah melanggar Pasal 340 KUHP tentang Pembunuhan Berencana berdasarkan putusan Mahkamah Agung nomor 1993 K/PID.SUS/2019 yang terdaftar pada tanggal 1 Januari 2019. Pengadilan memutuskan bersalah, dan menjatuhkan hukuman pidana penjara selama 20 tahun.'
+    ```
+"""
+
+@app.route('/static/<filename>')
+def static_pdf(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/', methods=['GET', 'POST'])
+def manage_pdfs():
+    extracted_text = None
+    summary_result = None
+    selected_pdf_path = None
+    pdf_path = None
+    conclusion = None
+    pdf_count = count_pdfs(app.config['UPLOAD_FOLDER'])
+    pdf_count_today = count_pdfs_today(app.config['UPLOAD_FOLDER'])
+    pdf_count_last_three_months = count_pdfs_last_three_months(app.config['UPLOAD_FOLDER'])
+
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect('/')
+        file = request.files['file']
+        if file.filename == '':
+            return redirect('/')
+        if file and file.filename.lower().endswith('.pdf'):
+            pdf_filename = file.filename
+            pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], pdf_filename)
+            file.save(pdf_path)
+        return redirect('/')
+
+    search_query = request.args.get('search_query', '').lower()
+    pdf_files = [f for f in os.listdir(UPLOAD_FOLDER) if f.lower().endswith('.pdf')]
+    filtered_pdfs = [pdf for pdf in pdf_files if search_query in pdf.lower()]
+
+    pdf_list = []
+    for pdf in filtered_pdfs:
+        pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], pdf)
+        try:
+            pdf_reader = PdfReader(pdf_path)
+            total_pages = len(pdf_reader.pages)
+            pdf_list.append({"Berkas Putusan": pdf, "Total Halaman": total_pages})
+        except Exception as e:
+            print(f"Error reading {pdf}: {e}")
+            pdf_list.append({"Berkas Putusan": pdf, "Total Halaman": "Error"})
+    
+
+    pdf_df = pd.DataFrame(pdf_list)
+    pdf_df= pdf_df.sort_values(by='Berkas Putusan')
+    pdf_html_table = pdf_df.to_html(index=False, escape=False)
+
+    search_query = request.args.get('search_query', '')
+    filtered_pdfs = [pdf for pdf in pdf_files if search_query.lower() in pdf.lower()]
+    selected_pdf = request.args.get('selected_pdf', '')
+    embed_pdf = f'/static/{selected_pdf}'
+
+    if filtered_pdfs:
+        if selected_pdf in filtered_pdfs:
+            selected_pdf = request.args.get('selected_pdf', '')
+            pdf_path = os.path.join(UPLOAD_FOLDER, selected_pdf)
+            pdf_reader = PdfReader(pdf_path)
+            pdf_reader = PdfReader(pdf_path)
+            page_count = len(pdf_reader.pages)
+
+            # Use page_count to determine the range for extracting text
+            if page_count > 5:
+                extracted_text_first = extract_text_from_pdf(pdf_path, 0, 4)
+                extracted_text_last = extract_text_from_pdf(pdf_path, page_count - 5, page_count - 1)
+                extracted_text = extracted_text_first + "\n" + extracted_text_last
+            else:
+                extracted_text = extract_text_from_pdf(pdf_path, 0, page_count - 1)
+
+            if extracted_text:
+                client = OpenAI(api_key=api_key)
+                response = client.chat.completions.create(
+                    model="gpt-3.5-turbo-0125",
+                    response_format={"type": "json_object"},
+                    messages=[
+                        {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
+                        {"role": "user", "content": template.format(extracted_text)}
+                    ], 
+                    temperature= 0.2
+                )
+                data = json.loads(response.choices[0].message.content)
+                conclusion = data['Kesimpulan']
+                for key in data:
+                    if isinstance(data[key], list):
+                        str_val = ""
+                        for val in data[key]:
+                            str_val = str_val + '\u2022' + ' '+ val + ' '
+                        
+                        data[key] = str_val
+
+                df = pd.json_normalize(data)
+                #df = df.rename(columns={'Kesimpulan': 'Summary'})
+                df = df.T
+
+                df.columns = ["Hasil Putusan"]
+
+     
+                df.to_csv('data/data.csv')
+                df.to_json('PN.json', orient='records')
+                summary_result = df.to_html(classes="dataframe", header=True, index=True)
+
+                view_button = f'<button class="btn"  data-target="#viewModal" data-pdf="{embed_pdf}"></button>'
+                summary_result += view_button
+
+    return render_template(
+        'index.html',
+        pdf_table=pdf_html_table,
+        search_query=search_query,
+        filtered_pdfs=filtered_pdfs,
+        extracted_text=extracted_text,
+        selected_pdf_path=selected_pdf_path,
+        embed_pdf=embed_pdf,
+        pdf_path=pdf_path,
+        summary_result=summary_result,
+        conclusion=conclusion,
+        pdf_count=pdf_count,
+        pdf_count_today=pdf_count_today,
+        pdf_count_last_three_months=pdf_count_last_three_months
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
